ddb668c36c6f8a47a16a52cfc4ba9e9949cd4986
adelidev <adel.Terki2@outlook.Fr>
2021-01-08 19:39:15 +0100

    +

--- script.js
+++ script.js
@@ -5,22 +5,42 @@ class Player {
         this.exp = 0;
         this.gold = 0;
         this.maxHP = 100;
+        this.HP = this.maxHP;
         this.maxMP = 40;
+        this.MP = this.maxMP;
         this.str = 12;
         this.def = 10;
         this.int = 8;
     }
+    displaySelf() {
+        $('#player').append(
+            '<div class="info">' +
+            '<span id="player-name" class="name-title">' + this.name + '</span>' +
+            '<br><span>niv.<i id="player-lvl">' + this.lvl + '</i></span>' +
+            '<button id="player-stats">Stats</button>' +
+            '</div>' +
+            '<div class="container">' +
+            '    <div class="bar" id="player-hp"><span>HP: <i>' + this.HP + '</i></span></div>' +
+            '    <div class="bar" id="player-mp"><span>MP: <i>' + this.MP + '</i></span></div>' +
+            '</div>'
+        );
+    }
 }
-
 class Enemy {
     constructor(name, lvl, maxHP, maxMP, str, def, int) {
         this.name = name;
         this.lvl = lvl;
-        this.maxHP = maxHP;
-        this.maxMP = maxMP;
+        this.HP = maxHP;
+        this.MP = maxMP;
         this.str = str;
         this.def = def;
         this.int = int;
+    }
+    displaySelf(eNb) {
+        $('#enemies').append('<div class="enemy-card">' +
+            '<div class="bar" id="enemy-hp"><span>HP: <i>' + this.HP + '</i></span></div>' +
+            '<span id="enemy-name' + eNb + '" class="name-title">' + this.name + '</span>' +
+            '<br><span> niv. <i id="enemy-lvl">' + this.lvl + '</i></span></div>');
     }
 }
 const enemies = [{
@@ -28,56 +48,64 @@ const enemies = [{
     lvl: 3,
     str: 10,
     def: 5,
+    int: 13,
     maxHP: 80,
+    maxMP: 100,
     sorts: { 0: "Brulure", 1: "Siphon" }
 }, {
     name: "Chevalier possédé",
     lvl: 5,
     str: 17,
     def: 12,
+    int: 10,
     maxHP: 110,
+    maxMP: 50,
 }, {
     name: "Lapin enragé",
     lvl: 1,
     str: 4,
     def: 3,
+    int: 0,
     maxHP: 50,
+    maxMP: 0,
 }, {
     name: "Grand serpent",
     lvl: 8,
     str: 20,
     def: 8,
+    int: 15,
     maxHP: 150,
+    maxMP: 50,
 }, {
     name: "Lézard venimeux",
     lvl: 2,
     str: 6,
     def: 4,
+    int: 0,
     maxHP: 40,
+    maxMP: 10,
 }, {
     name: "Homme désespéré",
     lvl: 1,
     str: 6,
     def: 6,
+    int: 6,
     maxHP: 60,
+    maxMP: 0,
 }, ]
 
 function generateEnemy() {
-    var randomizer = Math.round(Math.random() * Math.floor(enemies.length - 1));
-    $('#enemies').append('<div class="enemy-card">' +
-        '<div class="bar" id="enemy-hp"><span>HP: <i>' + enemies[randomizer].maxHP + '</i></span></div>' +
-        '<span id="enemy-name">' + enemies[randomizer].name + '</span>' +
-        '<span> niv. <i id="enemy-lvl">' + enemies[randomizer].lvl + '</i></span></div>');
+    var encounters = [];
+    for (let i = 0; i < Math.round(Math.random() * Math.floor(3)) + 1; i++) {
+        var randomizer = Math.round(Math.random() * Math.floor(enemies.length - 1));
+        encounters[i] = new Enemy(enemies[randomizer].name, enemies[randomizer].lvl, enemies[randomizer].maxHP, enemies[randomizer].maxMP, enemies[randomizer].str, enemies[randomizer].def, enemies[randomizer].int);
+        encounters[i].displaySelf(i);
+    }
 }
-
-
-
-
 
 function gameStart() {
     var player = new Player($("#start-screen div input").val());
-
-    $('#player-name').html(player.name);
+    player.displaySelf();
     generateEnemy();
 }
 
