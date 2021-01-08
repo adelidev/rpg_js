7dc816a7070e8533f1a3d1c1a4e1156ad87ef113
adelidev <adel.terki2@outlook.fr>
2021-01-08 10:08:29 +0100

    ++

--- script.js
+++ script.js
@@ -13,56 +13,61 @@ class Player {
 }
 
 class Enemy {
-    constructor() {
-        this.name = "";
-        this.lvl = 1;
-        this.maxHP = 100;
-        this.maxMP = 40;
-        this.str = 12;
-        this.def = 10;
-        this.int = 8;
+    constructor(name, lvl, maxHP, maxMP, str, def, int) {
+        this.name = name;
+        this.lvl = lvl;
+        this.maxHP = maxHP;
+        this.maxMP = maxMP;
+        this.str = str;
+        this.def = def;
+        this.int = int;
     }
 }
+const enemies = [{
+    name: "Démon",
+    lvl: 3,
+    str: 10,
+    def: 5,
+    maxHP: 80,
+    sorts: { 0: "Brulure", 1: "Siphon" }
+}, {
+    name: "Chevalier possédé",
+    lvl: 5,
+    str: 17,
+    def: 12,
+    maxHP: 110,
+}, {
+    name: "Lapin enragé",
+    lvl: 1,
+    str: 4,
+    def: 3,
+    maxHP: 50,
+}, {
+    name: "Grand serpent",
+    lvl: 8,
+    str: 20,
+    def: 8,
+    maxHP: 150,
+}, {
+    name: "Lézard venimeux",
+    lvl: 2,
+    str: 6,
+    def: 4,
+    maxHP: 40,
+}, {
+    name: "Homme désespéré",
+    lvl: 1,
+    str: 6,
+    def: 6,
+    maxHP: 60,
+}, ]
 
 function generateEnemy() {
-    const enemies = [{
-        name: "Démon",
-        lvl: 3,
-        str: 10,
-        def: 5,
-        maxHP: 80,
-    }, {
-        name: "Chevalier possédé",
-        lvl: 5,
-        str: 17,
-        def: 12,
-        maxHP: 110,
-    }, {
-        name: "Lapin enragé",
-        lvl: 1,
-        str: 4,
-        def: 3,
-        maxHP: 50,
-    }, {
-        name: "Grand serpent",
-        lvl: 8,
-        str: 20,
-        def: 8,
-        maxHP: 150,
-    }, {
-        name: "Lézard venimeux",
-        lvl: 2,
-        str: 6,
-        def: 4,
-        maxHP: 40,
-    }, {
-        name: "Homme désespéré",
-        lvl: 1,
-        str: 6,
-        def: 6,
-        maxHP: 60,
-    }, ]
-    Math.floor(Math.random() * Math.floor(max))
+    var randomizer = Math.round(Math.random() * Math.floor(enemies.length - 1));
+    $('#enemies').append('<div class="enemy-card">' +
+        '<div class="bar" id="enemy-hp"><span>HP: <i>' + enemies[randomizer].maxHP + '</i></span></div>' +
+        '<span id="enemy-name">' + enemies[randomizer].name + '</span>' +
+        '<span> niv. <i id="enemy-lvl">' + enemies[randomizer].lvl + '</i></span></div>');
 }
 
 
@@ -71,7 +76,9 @@ function generateEnemy() {
 
 function gameStart() {
     var player = new Player($("#start-screen div input").val());
+
     $('#player-name').html(player.name);
+    generateEnemy();
 }
 
 //Welcome Screen
